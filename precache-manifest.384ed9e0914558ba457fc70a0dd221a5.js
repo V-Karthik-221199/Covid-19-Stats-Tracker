721228e0e6160391f7708e53efa34e95cc410649
V-Karthik-221199 <vkarthik221199@gmail.com>
2020-05-10 22:28:26 +0530

    Updates

---
+++ precache-manifest.384ed9e0914558ba457fc70a0dd221a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8525e227afd4ed545b8f1f17cf15f620",
+    "url": "/Covid-19-Stats-Tracker/index.html"
+  },
+  {
+    "revision": "3c2494e41975f4802926",
+    "url": "/Covid-19-Stats-Tracker/static/css/main.80782431.chunk.css"
+  },
+  {
+    "revision": "565404ec5fe798814827",
+    "url": "/Covid-19-Stats-Tracker/static/js/2.67ef0149.chunk.js"
+  },
+  {
+    "revision": "d1684a62724c4cc780ee71567960e85c",
+    "url": "/Covid-19-Stats-Tracker/static/js/2.67ef0149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c2494e41975f4802926",
+    "url": "/Covid-19-Stats-Tracker/static/js/main.5e24d5e2.chunk.js"
+  },
+  {
+    "revision": "54532902c76b561fcda1",
+    "url": "/Covid-19-Stats-Tracker/static/js/runtime-main.cc18adfe.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19-Stats-Tracker/static/media/covid-19.d7265326.png"
+  }
+]);
